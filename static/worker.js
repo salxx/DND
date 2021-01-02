62766983f36fd3f72b87ee1d4b100b880997cd50
Oliver Winter <oliver.winter@xitrust.com>
2021-01-02 16:18:18 +0100

    defined Interface for worker

---
+++ static/worker.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Message: {
+ *  dateTime: 2011-10-05T14:48:00.000Z, //(ISO 8601)
+ *  Message: "I roll [1d20]" //[1d20] is replaced by RNG Value by the dungeon master client
+ * }
+ * 
+ * Message Endpoints:
+ * -> sendMessage(Message msg)
+ * <- receiveMessage(Message[] msgs)
+ */
+
+if(window.Worker){
+
+
+
+
+
+
+
+
+}
