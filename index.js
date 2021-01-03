29cf8727cbf6b0d0d7b7bf9f531e29b87ee46c2f
Florian Patrick Löbl <floebl@student.tugraz.at>
2021-01-03 21:56:33 +0100

    added first websocket implementation

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 8080
+const port = 8080;
+
+const http = require('http');
+const WebSocket = require('ws');
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', function connection(webSocket) {
+    webSocket.on('message', function incoming(data) {
+        wss.clients.forEach(function each(player) {
+            if (player !== webSocket && player.readyState === WebSocket.OPEN) {
+                player.send(data);
+            }
+        })
+    })
+    webSocket.send('Ahoi');
+});
 
 app.use(express.static('static'));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
--- static/script.js
+++ static/script.js
@@ -2,19 +2,42 @@ var worker = new Worker('worker.js');
 
 var chatInput = document.getElementById("chatInput");
 var chatSendButton = document.getElementById("chatSendButton");
-var chatDisplay = document.getElementById("chatDisplay")
+var chatDisplay = document.getElementById("chatDisplay");
 
-chatSendButton.addEventListener("click", ()=>{
-    if(chatInput.value.trim()){
+let ws;
+
+if (ws) {
+    ws.onerror = ws.onopen = ws.onclose = null;
+    ws.close();
+}
+
+ws = new WebSocket('ws://localhost:8080');
+ws.onopen = () => {
+    console.log('Connection opened!');
+}
+ws.onmessage = ({ data }) => {
+    worker.postMessage({ type: "Message", message: { dateTime: new Date().toISOString(), message: data } });
+}
+ws.onclose = function () {
+    ws = null;
+}
+
+chatSendButton.addEventListener("click", () => {
+    if (chatInput.value.trim()) {
         var text = chatInput.value.trim();
         chatInput.value = "";
-        worker.postMessage({type: "Message", message: {dateTime: new Date().toISOString(), author: "Fel the Elf", message: text}});
+        if (!ws) {
+            showMessage("No websocket connection available!");
+            return;
+        }
+        ws.send(text);
+        worker.postMessage({ type: "Message", message: { dateTime: new Date().toISOString(), message: text } });
     }
 });
 
-worker.onmessage = (({data})=>{
-    if(data && data.type){
-        switch(data.type){
+worker.onmessage = (({ data }) => {
+    if (data && data.type) {
+        switch (data.type) {
             case "MessageHistory":
                 chatDisplay.innerHTML = JSON.stringify(data.messages);
                 // Formatting messages goes HERE
