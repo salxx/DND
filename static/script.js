a094128a96fd3ac8eb0d78a06ce4ef7932192af8
Alex <alexander.sartory@gmail.com>
2021-01-16 19:33:47 +0100

    canvas drag

--- static/script.js
+++ static/script.js
@@ -4,6 +4,14 @@ var nameInput = document.getElementById("nameInput");
 var chatInput = document.getElementById("chatInput");
 var chatSendButton = document.getElementById("chatSendButton");
 var chatDisplay = document.getElementById("chatDisplay");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var img = new Image();
+var img2 = new Image();
+const imgs = {};
+var mousePos;
+var click = false;
+var selected = 0;
 
 chatSendButton.addEventListener("click", () => {
     if (chatInput.value.trim()) {
@@ -30,3 +38,56 @@ worker.onmessage = (({ data }) => {
         }
     }
 })
+
+window.onload = function () {
+    img.src = "maps/canyon_map.jpg";
+    img2.src = "images/LTR.png";
+    imgs[0] = {image:img2, posX:(canvas.width / 2), posY:(canvas.height / 2)};
+    imgs[1] = {image:img2, posX:(canvas.width / 2), posY:(canvas.height / 2)};
+    imgs[2] = {image:img2, posX:(canvas.width / 2), posY:(canvas.height / 2)};
+    img.addEventListener("load", function () {
+        renderMap();
+    });
+}
+
+function renderMap() {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    Object.entries(imgs).forEach(([key,value]) => {
+        ctx.drawImage(value.image, value.posX, value.posY, value.image.width, value.image.height);
+    });
+}
+
+function updatePosition() {
+    imgs[selected].posX = mousePos.x - (imgs[selected].image.width / 2);
+    imgs[selected].posY = mousePos.y - (imgs[selected].image.height / 2);
+}
+
+
+function myClick(canvas) {
+    if(click)
+      click = false;
+    else {
+        Object.entries(imgs).forEach(([key,value]) => {
+            if(mousePos.x > value.posX && mousePos.x < value.posX + value.image.width && mousePos.y > value.posY && mousePos.y < value.posY + value.image.height) {
+                click = true;
+                selected = key;
+            }
+        });
+      }
+}
+
+canvas.addEventListener('mousemove', function(evt) {
+   mousePos = getMousePos(canvas, evt);
+   if(click)
+    updatePosition();
+   renderMap();
+  }, false)
+
+  function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
