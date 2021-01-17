ebd094cf3742ccc0810a5b5d4156ffe2bf15059e
Alex <alexander.sartory@gmail.com>
2021-01-17 13:37:38 +0100

    minor changes

--- static/worker.js
+++ static/worker.js
@@ -60,7 +60,7 @@ onmessage = ({ data }) => {
     }
 }
 
-let regex = new RegExp('^([0-9])[d]([0-9][0-9]?)$');
+let regex = new RegExp('^([0-9])[d]([0-9][0-9]?[0-9]?)$');
 
 function rollDiceRegexCheck(data) {
     const found = data.message.message.match(regex);
